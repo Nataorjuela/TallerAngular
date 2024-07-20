22fbad2073154e5b4432a1abb0cfb24df5891a12
nata orjuela <naorjuel@bancolombia.com.co>
2024-07-20 17:12:04 -0500

    creación de componentes y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./generales/home/home.module').then(m => m.HomeModule) }, { path: 'profile', loadChildren: () => import('./maestros/profile/profile.module').then(m => m.ProfileModule) }, { path: 'accounts', loadChildren: () => import('./maestros/accounts/accounts.module').then(m => m.AccountsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
