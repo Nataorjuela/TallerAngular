530eb8fa85317c6467ef199c521bb1632e43920e
nata orjuela <naorjuel@bancolombia.com.co>
2024-08-15 20:48:36 -0500

    mejora de estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import {MatMenuModule} from '@angular/material/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +8,9 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { SignupModule } from './generales/signup/signup.module';
 import { loggingInterceptor } from './interceptors/logging.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
@@ -20,8 +23,11 @@ import { MatButtonModule } from '@angular/material/button';
     FormsModule,
     SignupModule,
     MatMenuModule,
-    MatButtonModule
-
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [
     provideClientHydration(),
